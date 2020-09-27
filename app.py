53f41389d1350bc8c04dc1682b611be80d6718fb
Filip Mularski <mularskif@gmail.com>
2020-09-27 11:49:43 +0200

    http get/post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -27,6 +27,20 @@ def index():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        username = request.form['username']
+        return redirect(url_for('user', usr=username))
+
+
+@app.route('/<usr>')
+def user(usr):
+    return f'<h1>{usr}</h1>'
 
 
 if __name__ == '__main__':
