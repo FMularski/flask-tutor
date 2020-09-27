1f454238fad14efee2ed69868bb957dba7d12f30
Filip Mularski <mularskif@gmail.com>
2020-09-27 11:31:01 +0200

    base.html + bootstrap

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route('/<name>')
-def index(name):
-    last_name = name * 2
-    letters = list('abcdefgh')
-    return render_template('index.html', name=name, last_name=last_name, letters=letters)
+# @app.route('/<name>')
+# def index(name):
+#     last_name = name * 2
+#     letters = list('abcdefgh')
+#     return render_template('index.html', name=name, last_name=last_name, letters=letters)
 
 
 # @app.route('/users/<user_id>')
@@ -19,6 +19,15 @@ def index(name):
 # def admin():
 #     return redirect(url_for('user', user_id=10))
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
